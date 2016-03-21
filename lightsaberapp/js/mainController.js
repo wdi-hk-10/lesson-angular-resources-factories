6dbac6c65c45f56890d5a0ffb14d12c4f2f33e54
Willie Tong <tongsinyin@gmail.com>
2016-03-21 16:21:54 +0800

    typos

--- lightsaberapp/js/mainController.js
+++ lightsaberapp/js/mainController.js
@@ -2,7 +2,7 @@ var lightsaberApp = angular.module("LightsaberApp");
 
 lightsaberApp.controller("MainController", ['$scope', '$resource', function($scope, $resource) {
   // Blank new character for form
-  $scope.character = {}
+  $scope.character = {};
 
   // Obtain our resource class
   var Character = $resource('http://localhost:3000/characters/:id', {id: '@_id'}, {
@@ -14,7 +14,7 @@ lightsaberApp.controller("MainController", ['$scope', '$resource', function($sco
 
   // Fetch the clicked todo
   $scope.selectCharacter = function(character) {
-    self.selectedCharacter = Character.get({id: character._id});
+    $scope.selectedCharacter = Character.get({id: character._id});
   };
 
   // Save as a Constructor
@@ -52,61 +52,3 @@ lightsaberApp.controller("MainController", ['$scope', '$resource', function($sco
     self.character = character;
   }
 }]);
-
-/*
-MainController.$inject = ['$resource']
-function MainController($resource){
-  var self = this;
-
-  // Blank new character for form
-  this.character = {}
-
-  // Obtain our resource class
-  var Character = $resource('http://localhost:3000/characters/:id', {id: '@_id'}, {
-    'update': { method:'PUT' }
-  });
-
-  // Fetch all todos
-  this.characters = Character.query();
-
-  // Fetch the clicked todo
-  this.selectCharacter = function(character) {
-    self.selectedCharacter = Character.get({id: character._id});
-  };
-
-  // Save as a Constructor
-  // this.addCharacter = function() {
-  //   var character = new Character(self.character);
-  //   character.$save(function(){
-  //     self.characters.push(character);
-  //     self.character = {};
-  //   });
-  // };
-
-  // Create/Update a Character (Class Method)
-  this.addCharacter = function() {
-    if (self.character._id) {
-      Character.update(self.character, function(){
-        self.character = {};
-      });
-    } else {
-      Character.save(self.character, function(character) {
-        self.characters.push(character);
-        self.character = {}
-      });
-    }
-  };
-
-  // Delete a Character
-  this.deleteCharacter = function(character){
-    Character.delete({id: character._id});
-    var index = self.characters.indexOf(character);
-    self.characters.splice(index, 1);
-  }
-
-  // Fill the form to edit a Character
-  this.editCharacter = function(character){
-    self.character = character;
-  }
-}
-*/
